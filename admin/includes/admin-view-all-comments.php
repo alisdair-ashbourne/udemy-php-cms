9c6b41025614463a687fef7462d77cefbfef5333
Alisdair Frost <alisdair.frost@gmail.com>
2017-05-13 19:44:25 -0600

    More comments tweaks
    -Added column for changing approval status
    
    -Created linked post titles

--- admin/includes/admin-view-all-comments.php
+++ admin/includes/admin-view-all-comments.php
@@ -8,15 +8,17 @@
 ?>
 <table class="table table-bordered table-hover">
     <thead>
-    <tr>
-        <th>ID</th>
-        <th>Author</th>
-        <th>Email</th>
-        <th>Post Title</th>
-        <th>Comment</th>
-        <th>Status</th>
-        <th>Date</th>
-    </tr>
+        <tr>
+            <th>ID</th>
+            <th>Author</th>
+            <th>Email</th>
+            <th>Post Title</th>
+            <th>Comment</th>
+            <th>Status</th>
+            <th>Approval</th>
+            <th>Date</th>
+        </tr>
+    </thead>
     </tbody>
     <?php
     $query = "SELECT * FROM comments LIMIT 8 ";
@@ -32,29 +34,44 @@
         $comment_status = $row['comment_status'];
         $comment_date = $row['comment_date'];
 
+        $change_comment_status = function($comment_status, $comment_id) {
+           $comment_status=="Unapproved" ?
+               $change_comment_status = array('Approve','admin-comments.php?status=Approve&c_id=' . $comment_id ):
+               $change_comment_status = array('Unapproved','admin-comments.php?status=Unapproved&c_id=' . $comment_id );
+           return $change_comment_status;
+        };
+
+        if(isset($_GET['status'])){
+            $query = "UPDATE comments SET comment_status = '{$_GET['status']}' WHERE comment_id = '{$_GET['c_id']}' ";
+            $update_comment_status_query = mysqli_query($connection, $query);
+            check_query($update_comment_status_query);
+            header("Location: admin-comments.php");
+        }
+
         echo "<tr>"
             . "<td>$comment_id</td>"
             . "<td>$comment_author</td>"
             . "<td>$comment_email</td>";
 
-            $query = "SELECT * FROM posts WHERE post_id = {$comment_post_id} ";
-            $update_comment_query = mysqli_query($connection, $query);
-            check_query($update_comment_query);
+        $query = "SELECT * FROM posts WHERE post_id = {$comment_post_id} ";
+        $update_comment_query = mysqli_query($connection, $query);
+        check_query($update_comment_query);
 
-            $row = mysqli_fetch_assoc($update_comment_query);
-            $comment_title = $row['post_title'];
+        $row = mysqli_fetch_assoc($update_comment_query);
+        $comment_title = $row['post_title'];
 
-        echo "<td>$comment_title</td>"
-            . "<td>$comment_content</td>"
+        echo "<td><a href='../post.php?p_id=$comment_post_id'>$comment_title</td>";
+
+        echo "<td>$comment_content</td>"
             . "<td>$comment_status</td>"
+            . "<td><a href='{$change_comment_status($comment_status, $comment_id)[1]}'>{$change_comment_status($comment_status, $comment_id)[0]}</a></td>"
             . "<td>$comment_date</td>"
             // Leaving in EDIT button for editing status
-            . "<td class='text-center'><a class='btn btn-primary' href='?source=edit_comment&p_id=$comment_id'><i class='fa fa-pencil'></i></a></td>"
             . "<td class='text-center'><a class='btn btn-danger' href='?delete=$comment_id'><i class='fa fa-trash'></i></a></td>"
             . "</tr>";
     }
 
-    if(isset($_GET['delete'])) {
+    if (isset($_GET['delete'])) {
         $the_comment_id = $_GET['delete'];
         $query = "DELETE FROM comments WHERE comment_id = '{$the_comment_id}' ";
         $delete_query = mysqli_query($connection, $query);
@@ -64,4 +81,3 @@
     ?>
     <tbody>
 </table>
-</thead>
--- includes/comments.php
+++ includes/comments.php
@@ -19,21 +19,27 @@ if (isset($_GET['p_id'])) {
         $comment_author = $row['comment_author'];
         $comment_content = $row['comment_content'];
         $comment_date = $row['comment_date'];
-        ?>
+        $comment_status = $row['comment_status'];
 
-        <div class="media">
-            <a class="pull-left" href="#">
-                <img class="media-object" src="http://placehold.it/64x64" alt="">
-            </a>
-            <div class="media-body">
-                <h4 class="media-heading"><?php echo $comment_author; ?>
-                    <small><?php echo $comment_date; ?></small>
-                </h4>
-                <p><?php echo $comment_content; ?></p>
+        if ($comment_status == 'Unapproved') {
+            continue;
+        } else {
+            ?>
+
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading"><?php echo $comment_author; ?>
+                        <small><?php echo $comment_date; ?></small>
+                    </h4>
+                    <p><?php echo $comment_content; ?></p>
+                </div>
             </div>
-        </div>
 
-    <?php }
+        <?php }
+    }
 } ?>
 
 
